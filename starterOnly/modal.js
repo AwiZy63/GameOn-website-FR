3b7c92e437429ace5f65dae4983ae8320c444425
BuildTools <unconfigured@null.spigotmc.org>
2022-03-09 13:41:13 +0100

    Correction du HTML | Ajout de la fonction closeModal pour la fermeture de celui-ci

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -20,4 +20,46 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
+// close modal function
+const modalCloseButton = document.getElementById("modalCloseButton");
 
+modalCloseButton.addEventListener("click", closeModal);
+function closeModal() {
+  // set modal style to display none when close button clicked
+  modalbg.style.display = "none";
+}
+
+function validate(event) {
+  event.preventDefault();
+  console.log(event)
+  const firstName = document.getElementById("first").value;
+  const lastName = document.getElementById("last").value;
+  const email = document.getElementById("email").value;
+  const birthDate = document.getElementById("birthdate").value;
+  const quantity = document.getElementById("quantity").value;
+
+
+  if (firstName === "") {
+    // Firstname empty error
+    console.error("firstname field is required");
+  } else if (firstName.length < 2) {
+    // Firstname length error
+    console.error("firstname must be greater or equal to 2");
+  }
+
+  if (lastName === "") {
+    // lastname empty error
+    console.error("lastname field is required");
+  } else if (lastName.length < 2) {
+    // lastname length error
+    console.error("lastname must be greater or equal to 2");
+  }
+
+  if (email === "") {
+    // email empty error
+  }
+
+
+    console.log([firstName, lastName, email, birthDate, quantity])
+    console.log(email.split("@"))
+}
