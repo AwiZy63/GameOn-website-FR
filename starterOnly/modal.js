6e68d0262c71bd35484fbe2b867693277c08ec03
BuildTools <unconfigured@null.spigotmc.org>
2022-03-09 15:02:19 +0100

    Ajout des messages d'erreur et de leurs styles, ainsi que les diffentes conditions de vérifications

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -29,15 +29,40 @@ function closeModal() {
   modalbg.style.display = "none";
 }
 
+const reservationForm = document.getElementById("reservationForm");
+reservationForm.addEventListener("submit", (event) => validate(event))
+
 function validate(event) {
   event.preventDefault();
-  console.log(event)
+
+  console.log(event);
+
   const firstName = document.getElementById("first").value;
   const lastName = document.getElementById("last").value;
   const email = document.getElementById("email").value;
   const birthDate = document.getElementById("birthdate").value;
   const quantity = document.getElementById("quantity").value;
 
+  const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  const numberRegex = /^\d+$/;
+
+  const checkbox = document.querySelectorAll(".radio-input");
+
+  const checkBoxArray = Array.from(checkbox);
+  console.log(checkBoxArray)
+
+  const isChecked = checkBoxArray.filter(input => input.checked);
+
+  const selectedBirthDate = new Date(birthDate);
+  const currentDate = new Date(Date.now());
+
+  const error = {
+    "firstName": false,
+    "lastName": false,
+    "email": false,
+    "birthDate": false,
+    "quantity": false
+  };
 
   if (firstName === "") {
     // Firstname empty error
@@ -57,9 +82,28 @@ function validate(event) {
 
   if (email === "") {
     // email empty error
+  } else if (!email.match(emailRegex)) {
+    // email invalid error
+  }
+
+  if (selectedBirthDate.getFullYear() > currentDate.getFullYear() - 16) {
+    console.error("Vous devez avoir minimum 16ans pour participer à l'évènement.")
+  }
+
+  if (quantity === "") {
+    // quantity empty error
+  } else if (!quantity.match(numberRegex)) {
+    // quantity invalid error
+    console.error("La quantité doit être un nombre")
+  } else if (quantity < 0 || quantity > 99) {
+    console.error("La quantité doit être comprise entre 0 et 99")
+  }
+
+  if (isChecked.length <= 0) {
+    // checked input error
   }
 
 
-    console.log([firstName, lastName, email, birthDate, quantity])
-    console.log(email.split("@"))
+  console.log([firstName, lastName, email, birthDate, quantity])
+  console.log(email.split("@"))
 }
