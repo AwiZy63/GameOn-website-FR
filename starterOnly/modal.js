2c428c8b59de396ac85b25269ada37927313638f
AwiZy63 <awizy0609@gmail.com>
2022-03-09 23:05:21 +0100

    Validation du formulaire ainsi que modification du responsive (css), optimisation du code JavaScript

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -33,77 +33,136 @@ const reservationForm = document.getElementById("reservationForm");
 reservationForm.addEventListener("submit", (event) => validate(event))
 
 function validate(event) {
-  event.preventDefault();
 
-  console.log(event);
+  const firstName = document.getElementById("first");
+  const lastName = document.getElementById("last");
+  const email = document.getElementById("email");
+  const birthDate = document.getElementById("birthdate");
+  const quantity = document.getElementById("quantity");
+  const checkbox = document.querySelectorAll(".radio-input");
+  const cguCheckBox = document.getElementById("checkbox1");
 
-  const firstName = document.getElementById("first").value;
-  const lastName = document.getElementById("last").value;
-  const email = document.getElementById("email").value;
-  const birthDate = document.getElementById("birthdate").value;
-  const quantity = document.getElementById("quantity").value;
+  const data = {
+    firstName: firstName.value,
+    lastName: lastName.value,
+    email: email.value,
+    birthDate: birthDate.value,
+    quantity: quantity.value,
+  }
 
-  const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  let error = {};
+
+  const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   const numberRegex = /^\d+$/;
 
-  const checkbox = document.querySelectorAll(".radio-input");
+  const isChecked = Array.from(checkbox).filter(input => input.checked);
 
-  const checkBoxArray = Array.from(checkbox);
-  console.log(checkBoxArray)
-
-  const isChecked = checkBoxArray.filter(input => input.checked);
-
-  const selectedBirthDate = new Date(birthDate);
+  const selectedBirthDate = new Date(data.birthDate);
   const currentDate = new Date(Date.now());
 
-  const error = {
-    "firstName": false,
-    "lastName": false,
-    "email": false,
-    "birthDate": false,
-    "quantity": false
-  };
+  data.firstName === "" || data.firstName.length < 2 ? error.firstName = true : error.firstName = false;
+  data.lastName === "" || data.lastName.length < 2 ? error.lastName = true : error.lastName = false;
+  data.email === "" || !data.email.match(emailRegex) ? error.email = true : error.email = false;
+  !data.birthDate || selectedBirthDate.getFullYear() > currentDate.getFullYear() - 16 ? error.birthDate = true : error.birthDate = false;
+  data.quantity === "" || !data.quantity.match(numberRegex) || data.quantity < 0 || data.quantity > 99 ? error.quantity = true : error.quantity = false;
+  isChecked.length <= 0 ? error.location = true : error.location = false;
+  !cguCheckBox ? error.cgu = true : error.cgu = false;  
 
-  if (firstName === "") {
-    // Firstname empty error
-    console.error("firstname field is required");
-  } else if (firstName.length < 2) {
-    // Firstname length error
-    console.error("firstname must be greater or equal to 2");
+  // Error check and define firstname
+  const firstNameErrorLabel = document.getElementById("firstError");
+  if (error.firstName) {
+    if (firstName.classList[1] !== "error-input") {
+      firstName.classList.toggle("error-input");
+      firstNameErrorLabel.style.display = "inline-block";
+    }
+  } else {
+    if (firstName.classList[1] === "error-input") {
+      firstName.classList.toggle("error-input");
+      firstNameErrorLabel.style.display = "none";
+    }
   }
 
-  if (lastName === "") {
-    // lastname empty error
-    console.error("lastname field is required");
-  } else if (lastName.length < 2) {
-    // lastname length error
-    console.error("lastname must be greater or equal to 2");
+  // Error check and define lastname
+  const lastNameErrorLabel = document.getElementById("lastError");
+  if (error.lastName) {
+    if (lastName.classList[1] !== "error-input") {
+      lastName.classList.toggle("error-input");
+      lastNameErrorLabel.style.display = "inline-block";
+    }
+  } else {
+    if (lastName.classList[1] === "error-input") {
+      lastName.classList.toggle("error-input");
+      lastNameErrorLabel.style.display = "none";
+    }
   }
 
-  if (email === "") {
-    // email empty error
-  } else if (!email.match(emailRegex)) {
-    // email invalid error
+  // Error check and define email
+  const emailErrorLabel = document.getElementById("emailError");
+  if (error.email) {
+    if (email.classList[1] !== "error-input") {
+      email.classList.toggle("error-input");
+      emailErrorLabel.style.display = "inline-block";
+    }
+  } else {
+    if (email.classList[1] === "error-input") {
+      email.classList.toggle("error-input");
+      emailErrorLabel.style.display = "none";
+    }
   }
 
-  if (selectedBirthDate.getFullYear() > currentDate.getFullYear() - 16) {
-    console.error("Vous devez avoir minimum 16ans pour participer à l'évènement.")
+  // Error check and define birthDate
+  const birthDateErrorLabel = document.getElementById("birthDateError");
+  if (error.birthDate) {
+    if (birthDate.classList[1] !== "error-input") {
+      birthDate.classList.toggle("error-input");
+      birthDateErrorLabel.style.display = "inline-block";
+    }
+  } else {
+    if (birthDate.classList[1] === "error-input") {
+      birthDate.classList.toggle("error-input");
+      birthDateErrorLabel.style.display = "none";
+    }
   }
 
-  if (quantity === "") {
-    // quantity empty error
-  } else if (!quantity.match(numberRegex)) {
-    // quantity invalid error
-    console.error("La quantité doit être un nombre")
-  } else if (quantity < 0 || quantity > 99) {
-    console.error("La quantité doit être comprise entre 0 et 99")
+  // Error check and define quantity
+  const quantityErrorLabel = document.getElementById("quantityError");
+  if (error.quantity) {
+    if (quantity.classList[1] !== "error-input") {
+      quantity.classList.toggle("error-input");
+      quantityErrorLabel.style.display = "inline-block";
+    }
+  } else {
+    if (quantity.classList[1] === "error-input") {
+      quantity.classList.toggle("error-input");
+      quantityErrorLabel.style.display = "none";
+    }
   }
 
-  if (isChecked.length <= 0) {
-    // checked input error
+  // Error check and define location
+  const locationErrorLabel = document.getElementById("locationError");
+  if (error.location) {
+    if (locationErrorLabel.style.display !== "inline-block") {
+      locationErrorLabel.style.display = "inline-block";
+    }
+  } else {
+    if (locationErrorLabel.style.display === "inline-block") {
+      locationErrorLabel.style.display = "none";
+    }
   }
 
+  // Error check and define cgu
+  const cguErrorLabel = document.getElementById("cguErrorLabel");
+  if (error.cgu) {
+    if (cguErrorLabel.style.display !== "inline-block") {
+      cguErrorLabel.style.display = "inline-block";
+    }
+  } else {
+    if (cguErrorLabel.style.display === "inline-block") {
+      cguErrorLabel.style.display = "none";
+    }
+  }
 
-  console.log([firstName, lastName, email, birthDate, quantity])
-  console.log(email.split("@"))
+  !error.firstName && !error.lastName && !error.email && !error.birthDate && !error.location && !error.quantity && !error.cgu ? alert("Merci ! Votre réservation a été reçue.") : event.preventDefault();
+
+
 }
